4b5236652d6d39ad9d07f690de7229ebb92ebe2f
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-18 22:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+    let f=document.getElementsByName("fname")[0].value
+ let l=document.getElementsByName("lname")[0].value
+	alert(`${f} ${l}`)
 }
